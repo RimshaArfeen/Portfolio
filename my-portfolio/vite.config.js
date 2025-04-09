80cc87864ff522132e0f5ef439c7d36e5dacee4c
RimshaArfeen <rimshaarfeen61@gmail.com>
2025-04-09 20:42:16 +0500

    Added Vercel

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -1,12 +1,7 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'vite-plugin-tailwind'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
 })
-
